a81548f3ad1747dd405a02ac090805057da786dd
andrew362 <37003497+andrew362@users.noreply.github.com>
2018-06-15 18:09:40 +0200

    cleaning

--- script/script.js
+++ script/script.js
@@ -1,42 +1,29 @@
-document.addEventListener('DOMContentLoaded', function(){
+var loader = document.getElementById('loadBG');
+window.addEventListener('load', function () {
     var wrapper = document.getElementById('wrapper');
     var topLayer = wrapper.querySelector('.top');
     var handle = wrapper.querySelector('.handle');
     var skew = 0;
-    var dalta = 0;
-    
-    if(wrapper.className.indexOf('skewed') != -1){
-    skew = 1000;
-}
-
-
-wrapper.addEventListener('mousemove', function(e){
-    console.log(e.clientX);
-    delta = (e.clientX - window.innerWidth/2)*0.5 ;
-    
-    handle.style.left = e.clientX + delta + 'px';
-    topLayer.style.width = e.clientX + skew + delta + 'px';
-    
-})
-    
-});
-
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    autoHeight:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
+    var delta = 0;
+    var windowWidth = window.innerWidth;
+    loader.style.opacity = 0;
+    setTimeout(function () {
+        loader.classList.remove('loader');
+    }, 500);
+    if (windowWidth < '540') {
+        wrapper.classList.remove('skewed');
+        var skew = 10000;
+        var delta = 10000;
+        handle.style.left = delta + 'px';
+        topLayer.style.width = skew + delta + 'px';
     }
-})
-
+    if (wrapper.className.indexOf('skewed') != -1) {
+        skew = 1000;
+    }
+    wrapper.addEventListener('mousemove', function (e) {
+        console.log(e.clientX);
+        delta = (e.clientX - window.innerWidth / 2) * 0.5;
+        handle.style.left = e.clientX + delta + 'px';
+        topLayer.style.width = e.clientX + skew + delta + 'px';
+    })
+});
